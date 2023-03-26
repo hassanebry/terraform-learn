d5fb0808fb8d911a5b4e68a813716a2d0bd640be
Barry Hassane <hassane.barry@etu.univ-orleans.fr>
2023-03-26 16:13:24 +0200

    route new route table

--- main.tf
+++ main.tf
@@ -1,65 +1,50 @@
 provider "aws" {
-  region     = "eu-west-3"
+  region = "eu-west-3"
 }
 
-variable "subnet_cidr_block" {
-  type        = string
-  description = "subnet cidr block"
-  default = "10.0.10.0/24"
-}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
 
-variable "vpc_cidr_block" {
-  type        = string
-  description = "vpc cidr block"
-}
-
-variable "environment" {
-  type        = string
-  description = "deployment environment"
-}
-
-
-resource "aws_vpc" "dev-vpc" {
+resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    Name : var.environment,
-    vpc_env : "dev"
+    Name : "${var.env_prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id            = aws_vpc.dev-vpc.id
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id            = aws_vpc.myapp-vpc.id
   cidr_block        = var.subnet_cidr_block
-  availability_zone = "eu-west-3a"
+  availability_zone = var.avail_zone
   tags = {
-    Name : "dev-subnet-1"
+    Name : "${var.env_prefix}-subnet-1"
   }
 }
 
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
 
-data "aws_vpc" "existing_vpc" {
-  default = true
-}
-
-resource "aws_subnet" "dev-subnet-2" {
-  vpc_id            = data.aws_vpc.existing_vpc.id
-  cidr_block        = "172.31.48.0/20"
-  availability_zone = "eu-west-3a"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
   tags = {
-    Name : "default-subnet-2"
+    Name : "${var.env_prefix}-rtb"
   }
 }
 
-output "dev-vpc-id" {
-  value       = aws_vpc.dev-vpc.id
-  sensitive   = false
-  description = "description"
-  depends_on  = []
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name : "${var.env_prefix}-igw"
+  }
 }
 
-output "dev-subnet-1-id" {
-  value       = aws_subnet.dev-subnet-1.id
-  sensitive   = false
-  description = "description"
-  depends_on  = []
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id      = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
 }
+
+
